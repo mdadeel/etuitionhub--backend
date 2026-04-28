fb56d59fd6551f3e988582d9a0508facab53d533
mdadeel <mdadeel125@gmail.com>
2026-04-29 00:51:58 +0600

    feat: add MongoDB indexes to Tuition model to optimize query performance

--- models/Tuition.js
+++ models/Tuition.js
@@ -19,6 +19,16 @@ var tuitionSchema = new mongoose.Schema({
     assigned_tutor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
 
+// Indexes for query performance
+tuitionSchema.index({ status: 1 });
+tuitionSchema.index({ class_name: 1 });
+tuitionSchema.index({ location: 1 });
+tuitionSchema.index({ createdAt: -1 });
+tuitionSchema.index({ salary: -1 });
+tuitionSchema.index({ status: 1, location: 1 });
+tuitionSchema.index({ status: 1, class_name: 1 });
+tuitionSchema.index({ student_email: 1 });
+
 var Tuition = mongoose.model('Tuition', tuitionSchema);
 
 module.exports = Tuition;
